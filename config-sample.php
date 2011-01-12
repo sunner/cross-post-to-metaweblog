2d27060dc526a1db2c14f6e91bbc9c7c5993a5a5
Sun Zhigang <sunner@gmail.com>
2011-01-12 10:49:59 +0800

    Add new config-sample.php file

---
+++ config-sample.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+ * Edit the following data and rename the file to config.php 
+ */
+$P2M_URL = 'http://domain.name/xml-rpc/';
+$P2M_USERNAME = 'username';
+$P2M_PASSWORD = 'passw0rd';
+$P2M_APPEND = "Any message appended to the post. You can use $origin_link as the link to the source blog post";
+$P2M_PUBLISH = true; // if false, the post will be saved as draft
+?>
